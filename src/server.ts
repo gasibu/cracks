9cb5cbf2c9f5c9e6135eb14318cb781ba170666a
Ayi Muhammad Iqbal Nasuha <bonk007@gmail.com>
2025-11-26 00:22:54 +0700

    now should be ok

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,35 @@
 import http from "node:http";
 import app from './app';
 
+import { httpServerHandler } from 'cloudflare:node'
 
 const PORT = parseInt('3000', 10);
 
-export default ((): void => {
-  const server = http.createServer(app);
+app.listen(3000)
+export default httpServerHandler({ port: 3000 })
 
-  server.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
-  });
-
-// Graceful shutdown
-  const shutdown = (signal: string) => {
-    console.log(`\n${signal} received. Shutting down...`);
-    server.close(() => {
-      console.log('HTTP server closed.');
-      process.exit(0);
-    });
-
-    // Force exit if not closed in time
-    setTimeout(() => {
-      console.error('Forcing shutdown after timeout.');
-      process.exit(1);
-    }, 10_000).unref();
-  };
-
-  process.on('SIGINT', () => shutdown('SIGINT'));
-  process.on('SIGTERM', () => shutdown('SIGTERM'));
-})();
+// export default (() => {
+//   const server = http.createServer(app);
+//
+//   server.listen(PORT, () => {
+//     console.log(`Server listening on http://localhost:${PORT}`);
+//   });
+//
+// // Graceful shutdown
+//   const shutdown = (signal: string) => {
+//     console.log(`\n${signal} received. Shutting down...`);
+//     server.close(() => {
+//       console.log('HTTP server closed.');
+//       process.exit(0);
+//     });
+//
+//     // Force exit if not closed in time
+//     setTimeout(() => {
+//       console.error('Forcing shutdown after timeout.');
+//       process.exit(1);
+//     }, 10_000).unref();
+//   };
+//
+//   process.on('SIGINT', () => shutdown('SIGINT'));
+//   process.on('SIGTERM', () => shutdown('SIGTERM'));
+// })();
